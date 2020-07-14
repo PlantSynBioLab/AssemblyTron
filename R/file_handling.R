12b567478ae2996f55fd97cc0f0be5752ba33056
RCMiller-PSB <ryancm16@vt.edu>
2020-07-14 11:40:07 -0400

    Updated Oligo Synthesis isolation with addtional comments and a return

--- R/file_handling.R
+++ R/file_handling.R
@@ -3,6 +3,7 @@
 #' @param path a string representing the directory containing the j5 assembly
 #' file to parse. This function operates on any file ending with
 #' _combinatorial.csv by default.
+#' @param file
 #'
 #' @details
 #'
@@ -13,20 +14,20 @@
 #'
 #' @examples
 #' parse_j5("J5_EricZirkle_GH3_mCherry")
-parse_j5 <- function(path = getwd(), file = "_combinatorial.csv"){
+parse_j5 <- function(path = getwd(), file = "_combinatorial.csv")
+{
   #body
-  library(tidyverse)
-  library(readr)
-  J5Lines = readLines(file) #reads lines from J5 file to help with parsing
+  J5Lines <- readLines(file) #reads lines from J5 file to help with parsing
 
-  Oligo = grep(pattern = "Oligo Synthesis",J5Lines) #searches for first set of data needed from J5 file (Oligo Synthesis Data)
+  Oligo <- grep(pattern = "Oligo Synthesis",J5Lines) #searches for first set of data needed from J5 file (Oligo Synthesis Data)
   #returns line number for the Oligo Synthesis
 
   PCR <- grep(pattern = "PCR Reactions",J5Lines)#Searches for the title of the next set of data (PCR Reactions)
   #Returns the line number of this pattern
 
   #Reads the Oligo Synthesis portion of the CSV file
-  Oligo_Read <- read_csv(file, col_names = "ID Number", "Name", "Length", "Tm", "Tm (3' only)", "Cost", "Sequence", "Sequence (3' only)",skip = Oligo+1,n_max = PCR-Oligo-2)
+  Oligo_Read <- readr::read_csv(file, col_names = c("ID Number", "Name", "Length", "Tm", "Tm (3' only)", "Cost", "Sequence", "Sequence (3' only)"),skip = Oligo+1,n_max = PCR-Oligo-3)
 
+  return(Oligo_Read)
 
 }
