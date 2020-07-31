26b0da6c2bf18f51b6669c5c5980afe2f797ca3f
RCMiller-PSB <ryancm16@vt.edu>
2020-07-31 08:05:53 -0400

    Outlines for else if statements and continued work on separating based on assembly method

--- R/file_handling.R
+++ R/file_handling.R
@@ -29,7 +29,7 @@ parse_j5 <- function(path = getwd(), file = "_combinatorial.csv")
   #returns the line number of this pattern
 
   golden_gate <- grep(pattern = "Assembly Pieces (Golden Gate",j5lines, fixed = TRUE)#Searches for the next set of data if the assembly is golden gate
-  #returns line number for this pattern  ]
+  #returns line number for this pattern
 
   #Reads the Oligo Synthesis portion of the CSV file
   oligo_read <- readr::read_csv(file, col_names = c("ID Number", "Name", "Length", "Tm", "Tm (3' only)", "Cost", "Sequence", "Sequence (3' only)"),skip = Oligo+1,n_max = PCR-Oligo-3)
@@ -38,18 +38,21 @@ parse_j5 <- function(path = getwd(), file = "_combinatorial.csv")
   oligo_feather <- feather::write_feather(Oligo_Read)#Creates feather file for Oligo Synthesis information
 
   #Reads the PCR Reactions Portion of the CSV file
-  pcr_read <- if (gibson & golden_gate > 0) {
-  }
-  else if (gibson > 0) {
-    readr::read_csv(file, col_names = c("Reaction ID Number", "Primary Template", "Alternate Template", "Forward Oligo ID Number", "Forward Oligo Name", "Reverse Oligo ID Number","Reverse Oligo Name", "Notes", "Mean Oligo Tm", "Delta Oligo Tm", "Mean Oligo Tm (3' Only)", "Delta Oligo Tm (3'Only)", "Length", "Sequence"), skip = PCR+1,n_max = gibson-PCR-3)
-  }
-    else if (golden_gate > 0){
-
-  }
+  pcr_read <- readr::read_csv(file, col_names = c("Reaction ID Number", "Primary Template", "Alternate Template", "Forward Oligo ID Number", "Forward Oligo Name", "Reverse Oligo ID Number","Reverse Oligo Name", "Notes", "Mean Oligo Tm", "Delta Oligo Tm", "Mean Oligo Tm (3' Only)", "Delta Oligo Tm (3'Only)", "Length", "Sequence"), skip = PCR+1,n_max = gibson-PCR-3)
   pcr_feather <- feather::write_feather(PCR_Read)#creates feather file for the PCR reaction information
 
   #Reads the Assembly Pieces portion of the CSV file
-  Assembly_Read <- readr::read_csv(file,col_names = c())
+  Assembly_Read <- if (gibson & golden_gate > 0)
+  {
+
+  }
+  else if (gibson > 0) {
+    readr::read_csv(file, col_names = c("Reaction ID Number", "Primary Template", "Alternate Template", "Forward Oligo ID Number", "Forward Oligo Name", "Reverse Oligo ID Number","Reverse Oligo Name", "Notes", "Mean Oligo Tm", "Delta Oligo Tm", "Mean Oligo Tm (3' Only)", "Delta Oligo Tm (3'Only)", "Length", "Sequence"), skip = gibson+1,n_max = gibson-combinations-3)
+  }
+  else if (golden_gate > 0){
+    readr::read_csv(file, col_names = c("Reaction ID Number", "Primary Template", "Alternate Template", "Forward Oligo ID Number", "Forward Oligo Name", "Reverse Oligo ID Number","Reverse Oligo Name", "Notes", "Mean Oligo Tm", "Delta Oligo Tm", "Mean Oligo Tm (3' Only)", "Delta Oligo Tm (3'Only)", "Length", "Sequence"), skip = PCR+1,n_max = golden_gate-combinations-3)
+  }
+
 
 
 }
