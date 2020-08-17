50341c5108ab29231de9e50d508ab2143b4894ca
RCMiller-PSB <ryancm16@vt.edu>
2020-08-17 15:57:31 -0400

    File and path handling fixed and feather files are writable.

--- R/file_handling.R
+++ R/file_handling.R
@@ -13,10 +13,10 @@
 #' @import readr
 #'
 #' @examples
-#' parse_j5(path = "J5_EricZirkle_GH3_mCherry")
+#' parse_j5(path = "./J5_EricZirkle_GH3_mCherry")
 parse_j5 <- function(path = getwd(), file = "_combinatorial.csv")
 {
-  file <- list.files(path = path, pattern = file)
+  file <- list.files(path = path, pattern = file, full.names = TRUE)
   #body
   j5lines <- readLines(file) #reads lines from J5 file to help with parsing
 
@@ -36,7 +36,7 @@ parse_j5 <- function(path = getwd(), file = "_combinatorial.csv")
   #Reads the Oligo Synthesis portion of the CSV file
   oligo_read <- readr::read_csv(file, col_names = c("ID Number", "Name", "Length", "Tm", "Tm (3' only)", "Cost", "Sequence", "Sequence (3' only)"),skip = oligo+1,n_max = pcr-oligo-3)
   #return(oligo_read)
-  feather::write_feather(oligo_read, path = path)#Creates feather file for Oligo Synthesis information
+  feather::write_feather(oligo_read, path = paste0(path,"/oligo.feather"))#Creates feather file for Oligo Synthesis information
 
   #Reads the PCR Reactions Portion of the CSV file
   pcr_read <- if (length(gibson) & length(golden_gate) > 0){
@@ -46,7 +46,7 @@ parse_j5 <- function(path = getwd(), file = "_combinatorial.csv")
   } else if (length(gibson) > 0){
     readr::read_csv(file, col_names = c("Reaction ID Number", "Primary Template", "Alternate Template", "Forward Oligo ID Number", "Forward Oligo Name", "Reverse Oligo ID Number","Reverse Oligo Name", "Notes", "Mean Oligo Tm", "Delta Oligo Tm", "Mean Oligo Tm (3' Only)", "Delta Oligo Tm (3'Only)", "Length", "Sequence"), skip = pcr+1,n_max = gibson-pcr-3)
   }
-  feather::write_feather(pcr_read, path = path)#Creates feather file for Oligo Synthesis information
+  feather::write_feather(pcr_read, path = paste0(path,"/pcr.feather"))#Creates feather file for Oligo Synthesis information
 
 
   #Reads the Assembly Pieces portion of the CSV file
@@ -58,9 +58,10 @@ parse_j5 <- function(path = getwd(), file = "_combinatorial.csv")
    readr::read_csv(file, col_names = c("Reaction ID Number", "Reaction Type", "Type ID Number", "Part(s)" , "Relative Overlap Position", "Extra 5' CPEC bps", "Extra 3' CPEC bps","CPEC Tm Next", "Overlap with next (#bps)", "Overlap with Next (Sequence)", "Overlap with Next (Sequence Reverse Compliment)", "Sequence Length" , "Sequence" ), skip = gibson+1,n_max = combinations-gibson-3, guess_max = 100)
    }
   #return(assembly_read)
-  feather::write_feather(assembly_read, path = path)#Creates feather file for Oligo Synthesis information
+  feather::write_feather(assembly_read, path = paste0(path,"/assembly.feather"))#Creates feather file for Oligo Synthesis information
 
   combinations_read <- readr::read_csv(file, col_names = c("ID Number", "Name","Aseembly Method", "Part(s) Bin 0", "Assembly Piece ID Number Bin 0", "Part(s) Bin 1", "Assembly Piece ID Number Bin 1"),skip = combinations+1)
-  feather::write_feather(combinations_read, path = path)#Creates feather file for Oligo Synthesis information
+  feather::write_feather(combinations_read, path = paste0(path,"/combinations.feather"))#Creates feather file for Oligo Synthesis information
 
 }
+
