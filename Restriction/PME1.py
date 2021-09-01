3bb42a36ddcfeed8458fdb6404ff047c8d32d1a6
plantsynbio <42825197+plantsynbio@users.noreply.github.com>
2021-09-01 09:59:42 -0400

    Update PME1.py

--- Restriction/PME1.py
+++ Restriction/PME1.py
@@ -11,7 +11,7 @@ metadata = {'protocolName': 'PME1 Digestion',
 }
 print(metadata)
 
-os.chdir("/data/user_storage/digestion/")
+os.chdir("/data/user_storage/restriction/")
 os.getcwd()
 
 plasmid = pd.read_csv("20210802_pme1.csv",engine = 'python', encoding='utf-8-sig')
@@ -49,10 +49,22 @@ for i, row in plasmid.iterrows():
     plasmid.loc[i,'Digestion Tube'] = row2tube[str(i)]
 
 
+from opentrons import protocol_api
+
+#Metadata is a dictionary of data that is read by the server and returned to the opentrons app. 
+#give yourself credit. you are required to specify the 'apiLevel' herefrom opentrons import protocol_api
+metadata = {
+    'protocolName': 'PME1 Digestion',
+    'author': 'Mason Kellinger <masonkellinger@vt.edu> and John Bryant <jbryant2@vt.edu',
+    'description': 'Protocol for digesting plasmids that have PME1 cut sites',
+    'apiLevel': '2.10'
+}
+#print(metadata)
+
 #################################################################################
 #Commands
 #############################################################################
-def run(protocol: protocol_api.Protocol.Context):
+def run(protocol: protocol_api.ProtocolContext):
 
      ## Loading labware
     thermo = protocol.load_module('Thermocycler Module')
@@ -60,7 +72,11 @@ def run(protocol: protocol_api.Protocol.Context):
     
     tiprack1 = protocol.load_labware("opentrons_96_tiprack_10ul",1)
     tiprack2 = protocol.load_labware("opentrons_96_tiprack_300ul",2)
-    tuberack = protocol.load_labware("opentrons_24_tuberack_nest_2ml_snapcap",4)
+    #tuberack = protocol.load_labware("opentrons_24_tuberack_nest_2ml_snapcap",4)
+    temp_module = protocol.load_module('temperature module', 4)
+    cold_tuberack = temp_module.load_labware('opentrons_24_aluminumblock_nest_1.5ml_snapcap', label='Temperature-Controlled Tubes')
+    temp_module.set_temperature(4)
+    print(temp_module.temperature)
     
  ## Loading tools
     pipette_right = protocol.load_instrument('p300_single_gen2','right',tip_racks = [tiprack2])
@@ -74,7 +90,7 @@ def run(protocol: protocol_api.Protocol.Context):
 # pick up water -> dispense into pcr tube within thermocycler -> get rid of tip
     pipette_right.pick_up_tip()
     for i, row in plasmid.iterrows():
-        pipette_right.aspirate(plasmid.loc[i].at['Volume of Water'],tuberack['A1'],2)
+        pipette_right.aspirate(plasmid.loc[i].at['Volume of Water'],cold_tuberack['A1'],2)
         pipette_right.dispense(plasmid.loc[i].at['Volume of Water'],pcr[plasmid.loc[i].at['Digestion Tube']],2)
         pipette_right.blow_out()
 
@@ -84,7 +100,7 @@ def run(protocol: protocol_api.Protocol.Context):
 
     for i, row in plasmid.iterrows():
         pipette_left.pick_up_tip()
-        pipette_left.aspirate(plasmid.loc[i].at['Volume of Plasmid'],tuberack[plasmid.loc[i].at['Plasmid Location']],2) #location of plasmid
+        pipette_left.aspirate(plasmid.loc[i].at['Volume of Plasmid'],cold_tuberack[plasmid.loc[i].at['Plasmid Location']],2) #location of plasmid
         pipette_left.dispense(plasmid.loc[i].at['Volume of Plasmid'],pcr[plasmid.loc[i].at['Digestion Tube']],2)
         pipette_left.drop_tip()
 
@@ -92,7 +108,7 @@ def run(protocol: protocol_api.Protocol.Context):
 
     for i, row in plasmid.iterrows():
         pipette_left.pick_up_tip()
-        pipette_left.aspirate(plasmid.loc[i].at['Buffer'],tuberack['A3'],2)
+        pipette_left.aspirate(plasmid.loc[i].at['Buffer'],cold_tuberack['A3'],2)
         pipette_left.dispense(plasmid.loc[i].at['Buffer'],pcr[plasmid.loc[i].at['Digestion Tube']],2)
         pipette_left.blow_out()
         pipette_left.drop_tip()  
@@ -102,7 +118,7 @@ def run(protocol: protocol_api.Protocol.Context):
 
     for i, row in plasmid.iterrows():
         pipette_left.pick_up_tip()
-        pipette_left.aspirate(plasmid.loc[i].at['PME1'],tuberack['A4'],2)
+        pipette_left.aspirate(plasmid.loc[i].at['PME1'],cold_tuberack['A4'],2)
         pipette_left.dispense(plasmid.loc[i].at['PME1'],pcr[plasmid.loc[i].at['Digestion Tube']],2)
         pipette_left.blow_out()
         pipette_left.drop_tip()
