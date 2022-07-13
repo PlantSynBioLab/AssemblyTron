c459749223c722b06c637b07c7c9c05208d89d20
jbryantvt <65429312+jbryantvt@users.noreply.github.com>
2022-07-13 13:19:14 -0400

    update

---
+++ setup.py
@@ -0,0 +1,27 @@
+#[metadata]
+from setuptools import setup, find_packages
+
+setup(
+    name = "DNA-AssemblyLine",
+    version = "0.0.2",
+    author = "John Bryant",
+    author_email = "jbryant2@vt.edu",
+    description = "A package for automating DNA assembly with an Opentrons liquid handling robot",
+    long_description = "file: README.md",
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/PlantSynBioLab/opentrons",
+    packages=find_packages(where="src"),
+#    Bug Tracker = []
+    classifiers =[
+        "Programming Language :: Python :: 3",
+    	"Operating System :: OS Independent",
+	"license :: Apache License 2.0"],
+
+#[options]
+#package_dir =
+#    = src
+#packages = find:
+    python_requires = ">=3.6")
+
+#[options.packages.find]
+#where = src
