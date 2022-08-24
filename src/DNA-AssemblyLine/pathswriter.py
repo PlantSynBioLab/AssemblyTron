f781c426e7c9a08982855ad1d39ce2141732bfbb
plantsynbio <42825197+plantsynbio@users.noreply.github.com>
2022-08-24 18:17:38 -0400

    Update pathswriter.py

--- src/DNA-AssemblyLine/pathswriter.py
+++ src/DNA-AssemblyLine/pathswriter.py
@@ -7,28 +7,3 @@ path = os.getcwd()
 df = pd.DataFrame({'opentrons_repo': [path]})
 
 df.to_csv("paths.csv")
-
-from tkinter import filedialog
-from tkinter import *
-
-def browse_button():
-    # Allow user to select a directory and store it in global var
-    # called folder_path
-    global folder_path
-    global name
-    filename = filedialog.askdirectory()
-    folder_path.set(filename)
-    print(filename)
-    name = filename
-
-root = Tk()
-root.geometry("800x800")
-folder_path = StringVar()
-lbl1 = Label(master=root,textvariable=folder_path)
-lbl1.grid(row=0, column=1)
-button2 = Button(text="Browse", command=browse_button)
-button2.grid(row=0, column=3)
-
-
-
-mainloop()
