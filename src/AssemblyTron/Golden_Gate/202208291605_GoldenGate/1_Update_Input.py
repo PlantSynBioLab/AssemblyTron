fc8709867d566f0223f48a84690b8087e835550f
plantsynbio <42825197+plantsynbio@users.noreply.github.com>
2022-08-29 16:36:20 -0400

    updated Copy Cloning with correct path and made the Update_Input.py actually work

--- src/AssemblyTron/Golden_Gate/202208291605_GoldenGate/1_Update_Input.py
+++ src/AssemblyTron/Golden_Gate/202208291605_GoldenGate/1_Update_Input.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 import subprocess
+import pandas
 
 def walk_up_folder(path, depth=1):
     _cur_depth = 1        
@@ -10,6 +11,9 @@ def walk_up_folder(path, depth=1):
     return path   
 
 shutil.copy2(os.getcwd()+'/Input.csv', walk_up_folder(os.getcwd(), 2))
+
+paths = pandas.read_csv(walk_up_folder(os.getcwd(), 3)+'/paths.csv')
+paths
 
 rc = subprocess.call([paths.loc[0].at['opentrons_repo']+'/Copy Cloning.bat'])
 rc
