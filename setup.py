4728f96eef2a627ab01f636a060b4f558d575d52
jbryantvt <65429312+jbryantvt@users.noreply.github.com>
2022-08-29 09:14:57 -0400

    changed software name to "AssemblyTron"

--- setup.py
+++ setup.py
@@ -1,29 +1,34 @@
 #[metadata]
-from setuptools import setup, find_packages
+from setuptools import setup
+from setuptools import find_packages
 
 setup(
-    name = "DNA-AssemblyLine",
-    version = "0.0.5",
+    name = "AssemblyTron",
+    version = "0.0.1",
     author = "John Bryant",
     author_email = "jbryant2@vt.edu",
     description = "A package for automating DNA assembly with an Opentrons liquid handling robot",
     long_description = "file: README.md",
     long_description_content_type = "text/markdown",
     url = "https://github.com/PlantSynBioLab/opentrons",
-    packages=find_packages(where="DNA-AssemblyLine"),
+    packages=find_packages(where="AssemblyTron"),
 #    Bug Tracker = []
     classifiers =[
         "Programming Language :: Python :: 3",
     	"Operating System :: OS Independent",
 	    "License :: OSI Approved :: Apache Software License"],
-    package_data={'': ['*.r', '*.R']},
+    package_data={'': ['*.r','*.bat','*.sh']},
     include_package_data=True,
 
 #[options]
 #package_dir =
 #    = src
 #packages = find:
-    python_requires = ">=3.6")
+    python_requires = ">=3.6",
+    install_requires=[
+      'pandas', 'os','shutil','numpy','subprocess','datetime','tkinter','csv'
+  ],
+  )
 
 #[options.packages.find]
 #where = src
