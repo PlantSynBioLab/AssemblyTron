fd690f83596bbe2ac6d774c20356be26a1add02e
plantsynbio <42825197+plantsynbio@users.noreply.github.com>
2022-08-29 15:26:36 -0400

    updated

--- src/DNA-AssemblyLine/Copy_Cloning.sh
+++ src/DNA-AssemblyLine/Copy_Cloning.sh
@@ -3,7 +3,7 @@ scp -r /mnt/c/Users/Public/Documents/opentrons/src/DNA-AssemblyLine/Cloning/* ro
 scp -r /mnt/c/Users/Public/Documents/opentrons/src/DNA-AssemblyLine/Restriction/* root@robot:/data/user_storage/Restriction/
 scp -r root@robot:/data/user_storage/*/*/output_*.csv /mnt/c/Users/Public/Documents/opentrons/src/DNA-AssemblyLine/outputs
 scp -r /mnt/c/Users/Public/Documents/opentrons/src/DNA-AssemblyLine/Golden_Gate/* root@robot:/data/user_storage/Golden_Gate/
-scp -r /mnt/c/Users/Public/Documents/opentrons/src/DNA-AssemblyLine/robotpaths.csv root@robot:/data/user_storage/
+scp -r /mnt/c/Users/Public/Documents/opentrons/src/DNA-AssemblyLine/paths.csv root@robot:/data/user_storage/
 scp -r /mnt/c/Users/Public/Documents/opentrons/src/DNA-AssemblyLine/output_new_pcrwell_location.csv root@robot:/data/user_storage/
 scp -r /mnt/c/Users/Public/Documents/opentrons/src/DNA-AssemblyLine/Cloning/Input.csv root@robot:/data/user_storage/Cloning/
 scp -r /mnt/c/Users/Public/Documents/opentrons/src/DNA-AssemblyLine/Golden_Gate/Input.csv root@robot:/data/user_storage/Golden_Gate/
