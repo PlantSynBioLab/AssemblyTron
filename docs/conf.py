344d16dbea411346f50fd4a361ca2bb02b1e819f
jbryantvt <65429312+jbryantvt@users.noreply.github.com>
2023-12-21 15:45:53 -0500

    Update documentation

--- docs/conf.py
+++ docs/conf.py
@@ -3,18 +3,63 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+import src.AssemblyTron.j5_to_csvs
+import src.AssemblyTron.j5_to_csvs_digests
+import src.AssemblyTron.opentrons_demo_script
+import src.AssemblyTron.pathswriter
+import src.AssemblyTron.Update_Input
+import src.AssemblyTron.static_wired_IP_address
+import src.AssemblyTron
+
+import src.AssemblyTron.Golden_Gate.dilution_24_digests_writer
+import src.AssemblyTron.Golden_Gate.dilution_24_writer
+import src.AssemblyTron.Golden_Gate.dilution_96_writer
+import src.AssemblyTron.Golden_Gate.GoldenGate_digests_separatepcrruns_gradient_writer
+import src.AssemblyTron.Golden_Gate.GoldenGate_nodigests_separatepcrruns_gradient_writer
+import src.AssemblyTron.Golden_Gate.Setup_digests_gradient
+import src.AssemblyTron.Golden_Gate.Setup_nodigests_seppcr_gradient_24
+import src.AssemblyTron.Golden_Gate.Setup_nodigests_seppcr_gradient_96
+
+import src.AssemblyTron.Cloning.dilution_24_writer
+import src.AssemblyTron.Cloning.dilution_96_writer
+import src.AssemblyTron.Cloning.IVA_separatepcrruns_gradient_writer
+import src.AssemblyTron.Cloning.Setup_seppcr_gradient_24
+import src.AssemblyTron.Cloning.Setup_seppcr_gradient_96
+
+import src.AssemblyTron.Error_prone_PCR_Golden_Gate._1_Setup_STD_GoldenGate_digests_PCR
+import src.AssemblyTron.Error_prone_PCR_Golden_Gate._1_Setup_STD_GoldenGate_PCR
+import src.AssemblyTron.Error_prone_PCR_Golden_Gate.dilution_24_digests_writer
+import src.AssemblyTron.Error_prone_PCR_Golden_Gate.dilution_Error_prone_PCR_writer
+import src.AssemblyTron.Error_prone_PCR_Golden_Gate.Error_prone_PCR_writer
+import src.AssemblyTron.Error_prone_PCR_Golden_Gate.GoldenGate_digests_separatepcrruns_gradient_writer
+import src.AssemblyTron.Error_prone_PCR_Golden_Gate.GoldenGate_separatepcrruns_gradient_writer
+import src.AssemblyTron.Error_prone_PCR_Golden_Gate.Setup_Error_prone_PCR
+import src.AssemblyTron.Error_prone_PCR_Golden_Gate.Setup_Error_prone_PCR_NOdigest
+import src.AssemblyTron.Error_prone_PCR_Golden_Gate.Setup_STD_GoldenGate_Assembly
+import src.AssemblyTron.Error_prone_PCR_Golden_Gate._1_Setup_STD_GoldenGate_digests_PCR
+
+import src.AssemblyTron.MoClo_builder.MoClo_writer
+import src.AssemblyTron.MoClo_builder.Setup_MoClo
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'AssemblyTron'
-copyright = '2023, John Bryant'
+copyright = '2022, John Bryant'
 author = 'John Bryant'
 release = '0.1.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode'
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
